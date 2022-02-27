e7dc476541ca804f7f6b9ece83367f0c7179ada8
Anurag kumar <akanuragkumar712@gmail.com>
2022-02-27 18:17:15 +0530

    initial commit

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -130,10 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
-]
 
 django_heroku.settings(locals())
