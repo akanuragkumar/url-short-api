9c709957a942bfce04907b8f563fd38264048ca0
Anurag kumar <akanuragkumar712@gmail.com>
2022-02-28 10:32:36 +0530

    initial commit to develop

--- url_shortner/migrations/0001_initial.py
+++ url_shortner/migrations/0001_initial.py
@@ -15,6 +15,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('link', models.CharField(max_length=10000)),
                 ('uuid', models.CharField(max_length=10, unique=True)),
+                ('total_hit', models.IntegerField(default=0, blank=True)),
+                ('hourly_hit', models.IntegerField(default=0, blank=True))
             ],
         ),
     ]
