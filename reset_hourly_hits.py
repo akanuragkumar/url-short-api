bd8217817c164caacb6f0ea46edc1aecfa9d74b3
Anurag kumar <akanuragkumar712@gmail.com>
2022-02-28 13:28:52 +0530

    initial commit to develop

--- reset_hourly_hits.py
+++ reset_hourly_hits.py
@@ -2,12 +2,19 @@ import psycopg2
 
 
 def reset_hourly_hit():
+    # conn = psycopg2.connect(
+    #         host='ec2-44-194-113-156.compute-1amazonaws.com',
+    #         database='d92a234ocho7h5',
+    #         user='armqugxjzonbdg',
+    #         password='5e47355403d4e003ccc8e89d99bbfa7dabeaad5281ca1772f89759a99c70623a',
+    #         port='5432')
     conn = psycopg2.connect(
-            host='ec2-44-194-113-156.compute-1amazonaws.com',
-            database='d92a234ocho7h5',
+            dbname='d92a234ocho7h5',
             user='armqugxjzonbdg',
             password='5e47355403d4e003ccc8e89d99bbfa7dabeaad5281ca1772f89759a99c70623a',
-            port='5432')
+            host='ec2-44-194-113-156.compute-1amazonaws.com',
+            port='5432'
+            )
     cursor = conn.cursor()
     cursor.execute("""SELECT * FROM "shortner_url";""")
     url_objs = cursor.fetchall()
