b356d68cdf18482bcbc201bd9cba0486ff47df95
Anurag kumar <akanuragkumar712@gmail.com>
2022-02-28 09:06:00 +0530

    initial commit to develop

--- url_short_api/settings.py
+++ url_short_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--tend)2m$k%dz5r(bo3+p&jl)$ty)lm41qjla-6y&w4k*38^oz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'url-shortner-anurag.herokuapp.com']
 
